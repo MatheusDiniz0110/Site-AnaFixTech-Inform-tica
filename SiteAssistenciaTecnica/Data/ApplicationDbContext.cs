9daadee8d626259e7bdb1a9fa1f22b6ef35f649b
Matheus Diniz Tereza <matheus.diniz.t@outlook.com>
2026-07-22 08:30:06 -0300

    Adicionar arquivos de projeto.

---
+++ SiteAssistenciaTecnica/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SiteAssistenciaTecnica.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
